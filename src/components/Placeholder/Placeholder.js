4ef127bfa4e3bdfaf6aa8780e4e8f7d240104264
Adam Witalewski <awitalewski@cleeng.com>
2019-09-03 14:48:51 +0200

    refactor placeholder and replace default app contents

--- src/components/Placeholder/Placeholder.js
+++ src/components/Placeholder/Placeholder.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-const PlaceholderStyled = styled.div`
-  display: flex;
-
-  justify-content: center;
-
-  padding: 40px;
-
-  border: solid 1px black;
-`;
+import PlaceholderStyled from './PlaceholderStyled';
 
 const Placeholder = ({ label }) => (
   <PlaceholderStyled>This is {label} placeholder</PlaceholderStyled>
