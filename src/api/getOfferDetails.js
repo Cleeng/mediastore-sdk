7f3918eeec93b64c1b22becf454a13027dc9909b
Adam Witalewski <awitalewski@cleeng.com>
2019-09-27 11:42:54 +0200

    fix auth on offer display

--- src/api/getOfferDetails.js
+++ src/api/getOfferDetails.js
@@ -2,13 +2,17 @@ import { JWT_TOKEN_LOCAL_STORAGE_KEY } from '../util/Constants';
 import mockOfferDetails from '../components/Offer/__mocks__/offerDetails';
 
 const getOfferDetails = offerId =>
-  new Promise(resolve => {
+  new Promise((resolve, reject) => {
     // eslint-disable-next-line no-unused-vars
     const token = localStorage.getItem(JWT_TOKEN_LOCAL_STORAGE_KEY) || '';
-    resolve({
-      ...mockOfferDetails,
-      title: `Some test offer (ID: ${offerId})`
-    });
+    if (token) {
+      resolve({
+        ...mockOfferDetails,
+        title: `Some test offer (ID: ${offerId})`
+      });
+    } else {
+      reject(new Error('Unauthorized'));
+    }
   });
 
 export default getOfferDetails;
--- src/containers/OfferContainer.js
+++ src/containers/OfferContainer.js
@@ -1,31 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import getOfferDetails from '../api/getOfferDetails';
 import Offer from '../components/Offer';
 
 const OfferContainer = ({ offerId }) => {
   const [offerDetails, setOfferDetails] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     let isMounted = true;
-    getOfferDetails(offerId).then(offerDetailsResponse => {
-      if (isMounted) {
-        setOfferDetails(offerDetailsResponse);
-      }
-    });
+    getOfferDetails(offerId)
+      .then(offerDetailsResponse => {
+        if (isMounted) {
+          setOfferDetails(offerDetailsResponse);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err);
+        }
+      });
     return () => {
       isMounted = false;
     };
   }, []);
 
-  return offerDetails ? (
-    <Offer
-      offerDetails={offerDetails}
-      price={offerDetails.price}
-      priceBeforeDiscount={offerDetails.price}
-      couponApplied={false}
-    />
-  ) : null;
+  if (error) {
+    return <Redirect to="/login" />;
+  }
+  if (offerDetails) {
+    return (
+      <Offer
+        offerDetails={offerDetails}
+        price={offerDetails.price}
+        priceBeforeDiscount={offerDetails.price}
+        couponApplied={false}
+      />
+    );
+  }
+  return null;
 };
 
 OfferContainer.propTypes = {
