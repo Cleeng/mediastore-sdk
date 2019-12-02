a74994542dfbc57445a3dec6ad519236cd8cec1d
Katarzyna Zemka <kzemka@cleeng.com>
2019-12-02 12:26:12 +0100

    Temp prod config

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,11 +1,13 @@
 module.exports = {
   ENVIRONMENT_CONFIGURATION: {
     GB_API_URL: JSON.stringify(
-      'https://hc0f1jaa70.execute-api.eu-west-1.amazonaws.com/staging'
+      'https://ke4vfe4m32.execute-api.eu-west-1.amazonaws.com/dev'
     ),
     ADYEN_PUBLIC_KEY: {
-      'https://example.cleeng.com': JSON.stringify('pub.v2.xxx')
+      'https://gummybear-staging.cleeng.com': JSON.stringify(
+        'pub.v2.7814375173870120.aHR0cHM6Ly9ndW1teWJlYXItc3RhZ2luZy5jbGVlbmcuY29t.T9omOG5PPfYe0dkMnWB1pmS5TJ1H4UIkCZEyw7d7IEY'
+      )
     },
-    WEB_API: JSON.stringify('https://cleeng.com')
+    WEB_API: JSON.stringify('https://cleengtest:cl33test!@staging.cleeng.com')
   }
 };
