7dd5441d2a071ef727fd266f3707edea4c700475
Katarzyna Zemka <kzemka@cleeng.com>
2019-12-04 10:59:28 +0100

    Sandbox production

--- config/environments/production.js
+++ config/environments/production.js
@@ -3,7 +3,7 @@ module.exports = {
     GB_API_URL: JSON.stringify('https://connect-sandbox.cleeng.com'),
     ADYEN_PUBLIC_KEY: {
       'https://webstoresdk-demo.cleeng.com': JSON.stringify(
-        'pub.v2.7814375173870120.aHR0cDovL3dlYnN0b3Jlc2RrLWRlbW8uY2xlZW5nLmNvbQ.jhFD9Q1Pq5vjHmkL1o13YNGRqtOGHiT-fUcp26yHCJg'
+        'pub.v2.7814375173870120.aHR0cHM6Ly93ZWJzdG9yZXNkay1kZW1vLmNsZWVuZy5jb20.dNb3eHo2bbrSZv_j36_TT1EiKr_X2p1SjAXkCaoOKWc'
       )
     },
     WEB_API: JSON.stringify('https://sandbox.cleeng.com')
