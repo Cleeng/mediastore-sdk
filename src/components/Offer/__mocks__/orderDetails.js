d5c8646a7071fb515e3303751d9b7285cd955885
Pawel Kaczmarek <pkaczmarek@cleeng.com>
2020-06-16 10:58:38 +0200

    Gummybear offer component improvements

--- src/components/Offer/__mocks__/orderDetails.js
+++ src/components/Offer/__mocks__/orderDetails.js
@@ -2,10 +2,12 @@ const orderDetails = {
   priceBreakdown: {
     offerPrice: 20,
     discountedPrice: 16,
-    discountAmount: 4
+    discountAmount: 4,
+    taxValue: 4.6
   },
   discount: {
     applied: false
-  }
+  },
+  totalPrice: 20
 };
 export default orderDetails;
