673597a4974d4fe4f01a692b06dd10269742ad2a
Iwona Kułacz <ikulacz@cleeng.com>
2020-09-10 10:06:42 +0200

    [feat/CP-222] Adyen- migrate from origin key to client key

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,11 +1,7 @@
 module.exports = {
   ENVIRONMENT_CONFIGURATION: {
     GB_API_URL: JSON.stringify('https://connect-sandbox.cleeng.com'),
-    ADYEN_PUBLIC_KEY: {
-      'https://webstoresdk-demo.cleeng.com': JSON.stringify(
-        'pub.v2.7814375173870120.aHR0cHM6Ly93ZWJzdG9yZXNkay1kZW1vLmNsZWVuZy5jb20.dNb3eHo2bbrSZv_j36_TT1EiKr_X2p1SjAXkCaoOKWc'
-      )
-    },
+    ADYEN_CLIENT_KEY: JSON.stringify('test_I4OFGUUCEVB5TI222AS3N2Y2LY6PJM3K'),
     WEB_API: JSON.stringify('https://sandbox.cleeng.com')
   }
 };
--- config/environments/staging.tpl.js
+++ config/environments/staging.tpl.js
@@ -1,9 +1,7 @@
 module.exports = {
   ENVIRONMENT_CONFIGURATION: {
     GB_API_URL: JSON.stringify('$STAGING_GB_API_URL'),
-    ADYEN_PUBLIC_KEY: {
-      '$STAGING_ADYEN_PUBLIC_KEY_URL': JSON.stringify('$STAGING_ADYEN_PUBLIC_KEY')
-    },
+    ADYEN_CLIENT_KEY: JSON.stringify('$STAGING_ADYEN_CLIENT_KEY'),
     WEB_API: JSON.stringify('$STAGING_WEB_API')
   }
 };
