3ea0f1b0567f0977b3998f496accdd28f2d66ed7
Iwona Kułacz <ikulacz@cleeng.com>
2020-09-14 13:15:11 +0200

    [feat/CP-223] update readme file

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,7 +1,6 @@
 module.exports = {
   ENVIRONMENT_CONFIGURATION: {
     GB_API_URL: JSON.stringify('https://connect-sandbox.cleeng.com'),
-    ADYEN_CLIENT_KEY: JSON.stringify('test_I4OFGUUCEVB5TI222AS3N2Y2LY6PJM3K'),
-    WEB_API: JSON.stringify('https://sandbox.cleeng.com')
+    ADYEN_CLIENT_KEY: JSON.stringify('test_I4OFGUUCEVB5TI222AS3N2Y2LY6PJM3K')
   }
 };
--- config/environments/staging.tpl.js
+++ config/environments/staging.tpl.js
@@ -1,7 +1,6 @@
 module.exports = {
   ENVIRONMENT_CONFIGURATION: {
     GB_API_URL: JSON.stringify('$STAGING_GB_API_URL'),
-    ADYEN_CLIENT_KEY: JSON.stringify('$STAGING_ADYEN_CLIENT_KEY'),
-    WEB_API: JSON.stringify('$STAGING_WEB_API')
+    ADYEN_CLIENT_KEY: JSON.stringify('$STAGING_ADYEN_CLIENT_KEY')
   }
 };
