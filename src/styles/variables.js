8a8867eac2f51753764cd03ee589d0d6a6340f66
Iwona Kulacz <ikulacz@cleeng.com>
2020-09-16 14:55:56 +0200

    remove unused code

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,8 +1,3 @@
-export const MediumGrey = '#b7bfca';
-export const TextFieldBorderFilter =
-  'invert(87%) sepia(14%) saturate(243%) hue-rotate(213deg) brightness(96%) contrast(80%)';
-export const FocusColor = '#80b4ea';
-
 // My Account
 export const MyAccountBlue = '#031a6e';
 export const MyAccountContentColor = '#f8f9fc';
@@ -15,9 +10,7 @@ export const PaypalSecondaryColor = '#74B8F2';
 export const MyAccountMenu = '#f2f5fc';
 export const MyAccountMenuActive = '#43599c';
 
-export const PaymentButtonBgn = '#00112c';
-
-// New color pallet
+// Common colors
 export const ErrorColor = '#CB4477';
 export const BackgroundColor = '#F8FAFD';
 export const IconsColor = '#BCC3CD';
@@ -25,12 +18,18 @@ export const MainColor = '#515364';
 export const LineColor = '#D3DBE6';
 export const White = '#FFFFFF';
 export const ConfirmColor = '#4EB7A1';
+export const MediumGrey = '#b7bfca';
+export const TextFieldBorderFilter =
+  'invert(87%) sepia(14%) saturate(243%) hue-rotate(213deg) brightness(96%) contrast(80%)';
+export const FocusColor = '#80b4ea';
+export const PaymentButtonBgn = '#00112c';
 
 export const Weak = '#ea753c';
 export const Fair = '#e7ca27';
 export const Good = '#5da3bd';
 export const Strong = '#5db98f';
 
+// Fonts
 export const LargeFont = '25px';
 export const BigFont = '16px';
 export const MediumFont = '14px';
