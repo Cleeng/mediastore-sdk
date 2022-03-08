7fe7a04687ef09c79f70fec7ab756fe02b0e3100
Pawel Kaczmarek <pkaczmarek@cleeng.com>
2022-03-08 13:30:08 +0100

    Remove global styles

--- src/containers/PlanDetails/PlanDetailsStyled.js
+++ src/containers/PlanDetails/PlanDetailsStyled.js
@@ -4,5 +4,6 @@ import { BackgroundColor } from 'styles/variables';
 // eslint-disable-next-line import/prefer-default-export
 export const WrapStyled = styled.div`
   width: 100%;
+  padding: 20px;
   background: ${BackgroundColor};
 `;
