b830989b341403044ca29b32f9b8df5801a1edf2
Iwona Kulacz <ikulacz@cleeng.com>
2022-05-06 14:42:12 +0200

    use new OfferWrapper in Checkout component

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -6,7 +6,7 @@ import Register from 'components/RegisterPage';
 import PasswordReset from 'components/PasswordReset';
 import Capture from 'components/Capture/Capture';
 import CheckoutConsents from 'components/CheckoutConsents';
-import OfferContainer from 'containers/OfferContainer';
+import OfferWrapper from 'containers/OfferContainer/OfferWrapper';
 import ThankYouPage from 'components/ThankYouPage';
 import Auth from 'services/auth';
 
@@ -89,7 +89,7 @@ class Checkout extends Component {
         );
       case 4:
         return (
-          <OfferContainer
+          <OfferWrapper
             offerId={offerId}
             onSuccess={() => this.goToStep(CheckoutSteps.PURCHASE.nextStep)}
           />
