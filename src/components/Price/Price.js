7fedae552c14a519b179d24f187924a87f761c69
Iwona Kulacz <ikulacz@cleeng.com>
2022-08-30 08:48:28 +0200

    add option to add a additional label to price

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import formatNumber from 'util/formatNumber';
+import { Trans } from 'react-i18next';
 
 import {
   WrapperStyled,
   CurrencyStyled,
   PriceStyled,
   PeriodStyled,
-  InnerWrapper
+  InnerWrapper,
+  AdditionalLabelStyled
 } from './PriceStyled';
 
 const addSpaceAfterNumber = str => {
@@ -22,6 +24,9 @@ const Price = ({ currency, price, period }) => (
     <InnerWrapper>
       <CurrencyStyled>{currency}</CurrencyStyled>
       <PriceStyled>{formatNumber(price)}</PriceStyled>
+      <AdditionalLabelStyled>
+        <Trans i18nKey="price-additional-label" />
+      </AdditionalLabelStyled>
     </InnerWrapper>
     {period && (
       <PeriodStyled>/&nbsp;{addSpaceAfterNumber(period)}</PeriodStyled>
