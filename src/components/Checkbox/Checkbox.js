1f9aad8c304da635fdf0f6995283cc9e3302c014
Pawel Kaczmarek <pkaczmarek@cleeng.com>
2022-10-19 12:36:32 +0200

    Refactor Checkbox component

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
   CheckboxStyled,
@@ -7,62 +7,54 @@ import {
   ConsentDefinitionStyled
 } from './CheckboxStyled';
 
-class Checkbox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const {
-      children,
-      onClickFn,
-      error,
-      checked,
-      required,
-      isMyAccount,
-      className,
-      disabled,
-      isRadioButton
-    } = this.props;
-    return (
-      <CheckboxStyled
-        onClick={e => onClickFn(e, disabled)}
-        role="checkbox"
-        tabIndex="-1"
-        aria-checked="false"
+const Checkbox = ({
+  children,
+  onClickFn,
+  error,
+  checked,
+  required,
+  isMyAccount,
+  className,
+  disabled,
+  isRadioButton
+}) => {
+  return (
+    <CheckboxStyled
+      onClick={e => onClickFn(e, disabled)}
+      role="checkbox"
+      tabIndex="-1"
+      aria-checked="false"
+      checked={checked}
+      aria-label={children}
+      className={className}
+      disabled={disabled}
+    >
+      <CheckFrameStyled
+        error={error && required && !checked}
+        tabIndex="0"
+        onKeyDown={e => {
+          return e.keyCode === 32 ? onClickFn() : null;
+        }}
+        isMyAccount={isMyAccount}
+        isRadioButton={isRadioButton}
         checked={checked}
-        aria-label={children}
-        className={className}
-        disabled={disabled}
       >
-        <CheckFrameStyled
-          error={error && required && !checked}
-          tabIndex="0"
-          onKeyDown={e => {
-            return e.keyCode === 32 ? onClickFn() : null;
-          }}
-          isMyAccount={isMyAccount}
-          isRadioButton={isRadioButton}
-          checked={checked}
-        >
-          {checked && (
-            <CheckMarkStyled
-              isMyAccount={isMyAccount}
-              isRadioButton={isRadioButton}
-            />
-          )}
-        </CheckFrameStyled>
-        <ConsentDefinitionStyled
-          dangerouslySetInnerHTML={{
-            __html: `${children}${required && isMyAccount ? '*' : ''}`
-          }}
-          checked={checked}
-        />
-      </CheckboxStyled>
-    );
-  }
-}
+        {checked && (
+          <CheckMarkStyled
+            isMyAccount={isMyAccount}
+            isRadioButton={isRadioButton}
+          />
+        )}
+      </CheckFrameStyled>
+      <ConsentDefinitionStyled
+        dangerouslySetInnerHTML={{
+          __html: `${children}${required && isMyAccount ? '*' : ''}`
+        }}
+        checked={checked}
+      />
+    </CheckboxStyled>
+  );
+};
 
 Checkbox.propTypes = {
   checked: PropTypes.bool,
