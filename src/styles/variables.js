e7bcdd72827279ad09ef2b2a6f6c0f5b42183602
Iwona Kulacz <ikulacz@cleeng.com>
2022-12-12 08:54:11 +0100

    fix input styling

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -54,10 +54,12 @@ export const TextFieldBorderFilter =
 export const FocusColor = '#80b4ea';
 export const PaymentButtonBgn = '#00112c';
 
-export const Weak = '#ea753c';
-export const Fair = '#e7ca27';
-export const Good = '#5da3bd';
-export const Strong = '#5db98f';
+export const PasswordStrengthColors = {
+  Weak: '#ea753c',
+  Fair: '#e7ca27',
+  Good: '#5da3bd',
+  Strong: '#5db98f'
+};
 
 // Fonts
 export const LargeFont = '25px';
