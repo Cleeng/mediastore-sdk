b4ef4665443ad13eba968736407bf36320f9373e
Iwona Kulacz <ikulacz@cleeng.com>
2023-01-24 12:17:39 +0100

    try

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import Input from 'components/Input/Input';
+import Input from 'components/Input';
 
 describe('Input component', () => {
   test('should render correctly without props', () => {
