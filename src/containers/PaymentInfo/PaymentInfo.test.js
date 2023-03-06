5b583d75c864420bedf335b28db4545745eec4cd
Izabela Pęza <izabelapeza@Izabelas-MacBook-Air.local>
2023-03-06 15:00:08 +0100

    MSSDK-1260 - Fix PaymentInfo.test

--- src/containers/PaymentInfo/PaymentInfo.test.js
+++ src/containers/PaymentInfo/PaymentInfo.test.js
@@ -50,6 +50,13 @@ const store = (isOpen = false) => ({
         expiresAt: pastDate
       }
     ]
+  },
+  offers: {
+    offers: [],
+    pauseOffers: [],
+    pauseOffersIDs: [],
+    loading: false,
+    error: null
   }
 });
 
