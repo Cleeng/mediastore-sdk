1003dcc53567d489af5d3b97e8b85fcb1f4c65d4
Jakub Korczak <jakub.korczak@tsh.io>
2023-03-06 18:27:16 +0100

    MSSDK-1262 - clean eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,11 +50,10 @@ module.exports = {
       'error',
       {
         devDependencies: [
-          'test.{ts,tsx}', // repos with a single test file
-          'test-*.{ts,tsx}', // repos with multiple top-level test files
-          '**/*{.,_}{test,spec}.{ts,tsx}', // tests where the extension or filename suffix denotes that it is a test
-          '**/jest.config.ts', // jest config
-          '**/jest.setup.ts' // jest setup
+          'test.{ts,tsx}',
+          '**/*{.,_}{test,spec}.{ts,tsx}',
+          '**/jest.config.ts',
+          '**/jest.setup.ts'
         ],
         optionalDependencies: false
       }
