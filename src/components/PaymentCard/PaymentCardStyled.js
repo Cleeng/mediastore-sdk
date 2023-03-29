cf2ef13d8ccfb1133a928d3fd96414919b83a2b5
Filip Papiernik <45321473+LeCarteloo@users.noreply.github.com>
2023-03-29 12:15:28 +0200

    MSSDK-1296 fix payment details styling (#191)
    * MSSDK-1296 Change breakpoints in PaymentMethod and PaymentCard
    
    * MSSDK-1296 Move paypal holderName down, centre add icon, reduce margin in Card
    
    * MSSDK-1296 Change holderName style, center elements in PaymentCard
    
    * MSSDK-1296 Center horizontaly icons in MyAccountError

--- src/components/PaymentCard/PaymentCardStyled.js
+++ src/components/PaymentCard/PaymentCardStyled.js
@@ -4,7 +4,9 @@ import { mediaFrom } from 'styles/BreakPoints';
 
 export const CardStyled = styled.div.attrs(() => ({
   className: 'msd__payment-card'
-}))``;
+}))`
+  height: 100%;
+`;
 
 export const CardTypeStyled = styled.div.attrs(() => ({
   className: 'msd__payment-card__type'
@@ -33,8 +35,7 @@ export const HolderNameStyled = styled.div.attrs(() => ({
 }))`
   color: ${FontColor};
   font-size: 13px;
-  font-weight: 600;
-  line-height: 20px;
+  font-weight: 300;
 `;
 
 export const CardExpirationStyled = styled.div.attrs(() => ({
@@ -95,7 +96,7 @@ export const CardInfoStyled = styled.div.attrs(() => ({
   justify-content: space-between;
   margin-bottom: 30px;
 
-  ${mediaFrom.smallest`
+  ${mediaFrom.medium`
     margin-bottom: 0;
   `}
 `;
@@ -130,8 +131,10 @@ export const CardInfoWrapStyled = styled.div.attrs(() => ({
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  height: 100%;
 
-  ${mediaFrom.smallest`
+  ${mediaFrom.medium`
     flex-direction: row;
-  `}
+    align-items: center;
+  `};
 `;
