723c34f827c880bcb1b8b31405a16ea3fa74a0b7
Iwona Kulacz <ikulacz@cleeng.com>
2023-04-28 13:52:21 +0200

    change import names to uppercase

--- src/util/paymentMethodHelper.js
+++ src/util/paymentMethodHelper.js
@@ -2,9 +2,9 @@ import { ReactComponent as CardLogo } from 'assets/images/paymentMethods/card.sv
 import { ReactComponent as PaypalLogo } from 'assets/images/paymentMethods/PPicon.svg';
 import { ReactComponent as ApplePayLogo } from 'assets/images/paymentMethods/applePay.svg';
 import { ReactComponent as GooglePayLogo } from 'assets/images/paymentMethods/googlepay.svg';
-import { ReactComponent as idealLogo } from 'assets/images/paymentMethods/ideal-small.svg';
-import { ReactComponent as sofortLogo } from 'assets/images/paymentMethods/sofort-small.svg';
-import { ReactComponent as bancontactLogo } from 'assets/images/paymentMethods/bancontact-small.svg';
+import { ReactComponent as IdealLogo } from 'assets/images/paymentMethods/ideal-small.svg';
+import { ReactComponent as SofortLogo } from 'assets/images/paymentMethods/sofort-small.svg';
+import { ReactComponent as BancontactLogo } from 'assets/images/paymentMethods/bancontact-small.svg';
 
 export const supportedPaymentMethods = [
   'card',
@@ -52,10 +52,10 @@ export const logos = {
   paypal: PaypalLogo,
   applepay: ApplePayLogo,
   googlepay: GooglePayLogo,
-  ideal: idealLogo,
-  sofort: sofortLogo,
-  bancontact_card: bancontactLogo,
-  bancontact_mobile: bancontactLogo
+  ideal: IdealLogo,
+  sofort: SofortLogo,
+  bancontact_card: BancontactLogo,
+  bancontact_mobile: BancontactLogo
 };
 
 export default logos;
