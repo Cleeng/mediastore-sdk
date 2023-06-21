b33a23896cca550deddcfcb42220d1abf3a05235
m4tewoosh <mnowak@cleeng.com>
2023-06-21 09:57:49 +0200

    ts and translation fixes

--- src/components/FreeOffer/FreeOffer.types.ts
+++ src/components/FreeOffer/FreeOffer.types.ts
@@ -1,5 +1,5 @@
 import { OfferProps } from 'components/Offer/Offer.types';
 
 export type FreeOfferProps = {
-  onPaymentComplete: OfferProps['onCouponSubmit'];
+  onPaymentComplete: OfferProps['onPaymentComplete'];
 };
