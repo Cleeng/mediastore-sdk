a5b8107710d958a981e7870cb386523b98834eb4
Iwona Kulacz <ikulacz@cleeng.com>
2023-06-27 13:55:23 +0200

    simplify SubscriptionSwitches component

--- src/containers/SubscriptionSwitches/SubscriptionSwitches.component.js
+++ src/containers/SubscriptionSwitches/SubscriptionSwitches.component.js
@@ -3,11 +3,12 @@ import { useTranslation } from 'react-i18next';
 import { PropTypes } from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchOffers } from 'redux/offersSlice';
 import SectionHeader from 'components/SectionHeader';
 import SubscriptionSwitchesList from 'components/SubscriptionSwitchesList';
 import MyAccountError from 'components/MyAccountError';
 import PlanDetailsPopupManager from 'components/PlanDetailsPopupManager';
+import { showPopup } from 'redux/popupSlice';
+import { POPUP_TYPES } from 'redux/innerPopupReducer';
 
 import {
   fetchCustomerOffers,
@@ -20,24 +21,22 @@ import { WrapStyled } from './SubscriptionSwitchesStyled';
 const SubscriptionSwitches = ({
   offerId,
   onCancel,
+  toOfferId,
   onSwitchSuccess,
   onSwitchError
 }) => {
-  const { offerToSwitch } = useSelector(state => state.plan);
   const { isOpen: isPopupOpen } = useSelector(state => state.popupManager);
   const { data: switchSettings } = useSelector(
     store => store.plan.switchSettings
   );
-
   const [switchSettingsError, setSwitchSettingsError] = useState(false);
-  const { offers } = useSelector(state => state.offers);
 
   const dispatch = useDispatch();
 
   const { t } = useTranslation();
 
-  const getAndSaveSwitchSettings = async customerSubscriptions => {
-    await dispatch(fetchAvailableSwitches(customerSubscriptions));
+  const getAndSaveSwitchSettings = async selectedSubscriptionData => {
+    await dispatch(fetchAvailableSwitches([selectedSubscriptionData]));
   };
 
   const fetchOffersData = async () => {
@@ -53,26 +52,45 @@ const SubscriptionSwitches = ({
 
     dispatch(fetchPendingSwitches(offersWithPendingSwitches));
 
-    const selectedSubscription = activeSubscriptions.filter(
+    const selectedSubscriptionData = activeSubscriptions.find(
       offer => offer.offerId === offerId
     );
-
-    if (selectedSubscription.length === 1) {
-      dispatch(setOfferToSwitch(activeSubscriptions[0]));
-      getAndSaveSwitchSettings(activeSubscriptions);
-    } else {
+    if (!selectedSubscriptionData) {
       setSwitchSettingsError(true);
+      return;
+    }
+    dispatch(setOfferToSwitch(selectedSubscriptionData));
+    getAndSaveSwitchSettings(selectedSubscriptionData);
+  };
+
+  const immediatelyOpenSwitchPopupIfPossible = () => {
+    if (offerId && toOfferId) {
+      const toOfferData = switchSettings[offerId]?.available.find(
+        item => item.toOfferId === toOfferId
+      );
+      if (!toOfferData) return;
+      dispatch(
+        showPopup({
+          type: POPUP_TYPES.switchPlan,
+          data: {
+            offerData: {
+              ...toOfferData
+            }
+          }
+        })
+      );
     }
   };
 
   useEffect(() => {
-    if (offerId && !Object.keys(offerToSwitch).length) {
+    if (offerId) {
       fetchOffersData();
-    } else if (offerId && !Object.keys(switchSettings).length) {
-      getAndSaveSwitchSettings();
     }
-    if (offers.length === 0) dispatch(fetchOffers());
-  }, [offerId]);
+  }, []);
+
+  useEffect(() => {
+    immediatelyOpenSwitchPopupIfPossible();
+  }, [switchSettings]);
 
   if (switchSettingsError) {
     return <MyAccountError generalError />;
@@ -101,13 +119,15 @@ SubscriptionSwitches.propTypes = {
   offerId: PropTypes.string.isRequired,
   onCancel: PropTypes.func,
   onSwitchSuccess: PropTypes.func,
-  onSwitchError: PropTypes.func
+  onSwitchError: PropTypes.func,
+  toOfferId: PropTypes.string
 };
 
 SubscriptionSwitches.defaultProps = {
   onCancel: null,
   onSwitchSuccess: null,
-  onSwitchError: null
+  onSwitchError: null,
+  toOfferId: null
 };
 
 export { SubscriptionSwitches as PureSubscriptionSwitches };
