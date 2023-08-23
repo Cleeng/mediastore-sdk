5fac0154d9d8b2e2c4160dc8d9ce63a1d8d15958
kkarasek <jkarasek@cleeng.com>
2023-08-23 23:59:26 +0200

    refactor/MSSDK-1388: refactor OfferDetailDescription component

--- src/components/OfferDetailsDescription/OfferDetailsDescription.tsx
+++ src/components/OfferDetailsDescription/OfferDetailsDescription.tsx
@@ -36,181 +36,207 @@ const OfferDetailsDescription = ({
 }: OfferDetailsProps) => {
   const { t } = useTranslation();
 
-  const generateTrialDescription = () => {
-    if (period === 'season' && freeDays) {
-      const formattedDescription = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) after {{freeDays}} days and will be renewed on the next season start date.`;
+  const renderDescriptionForSubscription = () => {
+    const generateTrialDescription = () => {
+      if (period === 'season' && freeDays) {
+        const formattedDescription = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) after {{freeDays}} days and will be renewed on the next season start date.`;
+        return t(
+          `subscription-desc.trial-days.period-season`,
+          formattedDescription,
+          {
+            currencySymbol,
+            grossPrice,
+            taxCopy,
+            freeDays
+          }
+        );
+      }
+
+      if (freeDays) {
+        const description = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) after {{freeDays}} days.\nNext payments will occur every ${getReadablePeriod(
+          period
+        )}`;
+        return t(`subscription-desc.trial-days.period-${period}`, description, {
+          currencySymbol,
+          grossPrice,
+          taxCopy,
+          freeDays
+        });
+      }
+
+      // freePeriods
+      let formattedDescription =
+        'You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) ';
+      if (period === 'month') {
+        formattedDescription +=
+          freePeriods === 1
+            ? 'after month. </br>Next payments will occur every month.'
+            : 'after {{freePeriods}} months.\nNext payments will occur every month.';
+      } else if (period === 'week') {
+        formattedDescription +=
+          freePeriods === 1
+            ? 'after week. </br>Next payments will occur every week.'
+            : 'after {{freePeriods}} weeks.\n>Next payments will occur every week.';
+      }
       return t(
-        `subscription-desc.trial-days.period-season`,
+        `subscription-desc.trial-period${
+          freePeriods === 1 ? '' : 's'
+        }.period-${period}`,
         formattedDescription,
         {
           currencySymbol,
           grossPrice,
           taxCopy,
-          freeDays
+          freePeriods
         }
       );
-    }
+    };
 
-    if (freeDays) {
-      const description = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) after {{freeDays}} days.\nNext payments will occur every ${getReadablePeriod(
-        period
-      )}`;
-      return t(`subscription-desc.trial-days.period-${period}`, description, {
-        currencySymbol,
-        grossPrice,
-        taxCopy,
-        freeDays
-      });
-    }
-
-    // freePeriods
-    let formattedDescription =
-      'You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) ';
-    if (period === 'month') {
-      formattedDescription +=
-        freePeriods === 1
-          ? 'after month. </br>Next payments will occur every month.'
-          : 'after {{freePeriods}} months.\nNext payments will occur every month.';
-    } else if (period === 'week') {
-      formattedDescription +=
-        freePeriods === 1
-          ? 'after week. </br>Next payments will occur every week.'
-          : 'after {{freePeriods}} weeks.\n>Next payments will occur every week.';
-    }
-    return t(
-      `subscription-desc.trial-period${
-        freePeriods === 1 ? '' : 's'
-      }.period-${period}`,
-      formattedDescription,
-      {
-        currencySymbol,
-        grossPrice,
-        taxCopy,
-        freePeriods
-      }
-    );
-  };
-
-  const generateCouponDescription = () => {
-    const formattedTotalPrice = formatNumber(totalPrice);
-    const regularPrice = calculateGrossPriceForFreeOffer(
-      offerPrice,
-      taxRate,
-      customerPriceInclTax
-    );
-    if (discountedPeriods === 1) {
-      return t(
-        `subscription-desc-coupon-${period}`,
-        `You will be charged ${currencySymbol}${formattedTotalPrice} (incl. ${taxCopy}) now.\nAfter ${
-          period === '3months' || period === '6months' ? '' : 'a'
-        } ${periodMapper[period as Period]
-          .chargedForEveryText as string} you will be charged a regular price of ${currencySymbol}${regularPrice}.`,
-        { currencySymbol, formattedTotalPrice, taxCopy, period, regularPrice }
+    const generateCouponDescription = () => {
+      const formattedTotalPrice = formatNumber(totalPrice);
+      const regularPrice = calculateGrossPriceForFreeOffer(
+        offerPrice,
+        taxRate,
+        customerPriceInclTax
       );
-    }
-
-    // non-standard periods
-    if (period === '3months' || period === '6months') {
-      const description = `You will be charged ${currencySymbol}${formattedTotalPrice} (incl. ${taxCopy}) per ${periodMapper[
-        period as Period
-      ]
-        .chargedForEveryText as string} for the next ${discountedPeriods} billing periods.\nAfter that time you will be charged a regular price of ${currencySymbol}${regularPrice}.`;
-      return t(`subscription-desc-coupon.periods-${period}`, description, {
-        formattedTotalPrice,
-        taxCopy,
-        discountedPeriods,
-        currencySymbol,
-        regularPrice
-      });
-    }
-
-    return t(
-      `subscription-desc-coupon-${period}s`,
-      `You will be charged ${currencySymbol}${formattedTotalPrice} (incl. ${taxCopy}) per ${period} for the next ${discountedPeriods} ${period}s.\nAfter that time you will be charged a regular price of ${currencySymbol}${regularPrice}.`,
-      {
-        currencySymbol,
-        formattedTotalPrice,
-        taxCopy,
-        period,
-        discountedPeriods,
-        regularPrice
+      if (discountedPeriods === 1) {
+        return t(
+          `subscription-desc-coupon-${period}`,
+          `You will be charged ${currencySymbol}${formattedTotalPrice} (incl. ${taxCopy}) now.\nAfter ${
+            period === '3months' || period === '6months' ? '' : 'a'
+          } ${periodMapper[period as Period]
+            .chargedForEveryText as string} you will be charged a regular price of ${currencySymbol}${regularPrice}.`,
+          { currencySymbol, formattedTotalPrice, taxCopy, period, regularPrice }
+        );
       }
-    );
-  };
 
-  const generateSubscriptionDescription = () => {
-    if (discountType === 'coupon') {
-      return generateCouponDescription();
-    }
+      // non-standard periods
+      if (period === '3months' || period === '6months') {
+        const description = `You will be charged ${currencySymbol}${formattedTotalPrice} (incl. ${taxCopy}) per ${periodMapper[
+          period as Period
+        ]
+          .chargedForEveryText as string} for the next ${discountedPeriods} billing periods.\nAfter that time you will be charged a regular price of ${currencySymbol}${regularPrice}.`;
+        return t(`subscription-desc-coupon.periods-${period}`, description, {
+          formattedTotalPrice,
+          taxCopy,
+          discountedPeriods,
+          currencySymbol,
+          regularPrice
+        });
+      }
 
-    if (!isTrialAvailable) {
-      if (period === 'season') {
-        const description = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) and will be renewed on the next season start date.`;
-        return t(`subscription-desc.period-season`, description, {
+      return t(
+        `subscription-desc-coupon-${period}s`,
+        `You will be charged ${currencySymbol}${formattedTotalPrice} (incl. ${taxCopy}) per ${period} for the next ${discountedPeriods} ${period}s.\nAfter that time you will be charged a regular price of ${currencySymbol}${regularPrice}.`,
+        {
+          currencySymbol,
+          formattedTotalPrice,
+          taxCopy,
+          period,
+          discountedPeriods,
+          regularPrice
+        }
+      );
+    };
+
+    const generateSubscriptionDescription = () => {
+      if (discountType === 'coupon') {
+        return generateCouponDescription();
+      }
+
+      if (!isTrialAvailable) {
+        if (period === 'season') {
+          const description = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) and will be renewed on the next season start date.`;
+          return t(`subscription-desc.period-season`, description, {
+            currencySymbol,
+            grossPrice,
+            taxCopy
+          });
+        }
+        const formattedDescription = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) every ${getReadablePeriod(
+          period
+        )}`;
+        return t(`subscription-desc.period-${period}`, formattedDescription, {
           currencySymbol,
           grossPrice,
           taxCopy
         });
       }
-      const formattedDescription = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) every ${getReadablePeriod(
-        period
-      )}`;
-      return t(`subscription-desc.period-${period}`, formattedDescription, {
-        currencySymbol,
-        grossPrice,
-        taxCopy
-      });
-    }
 
-    return generateTrialDescription();
-  };
+      return generateTrialDescription();
+    };
 
-  if (offerType === 'S') {
-    const icons = [<CreditCardIcon />, <ClockIcon />];
     const description = generateSubscriptionDescription();
-    return (
-      <DetailsWrapper>
-        {description.split('\n').map((item, index) => (
+    const descriptionLines = description.split('\n');
+    if (descriptionLines.length !== 1) {
+      return (
+        <DetailsWrapper>
           <DetailsStyled>
             <LineWrapperStyled>
-              <IconStyled>{icons[index]}</IconStyled> {item}
+              <IconStyled>
+                <CreditCardIcon />
+              </IconStyled>
+              {descriptionLines[0]}
             </LineWrapperStyled>
           </DetailsStyled>
-        ))}
-      </DetailsWrapper>
-    );
-  }
-  if (offerType === 'P') {
-    const icon = <CalendarIcon />;
-    if (!period) {
-      const date = dateFormat(expiresAt, true);
-      const description = t('pass-desc.date', `Access until {{date}}`, {
-        date
-      });
-      return (
+          <DetailsStyled>
+            <LineWrapperStyled>
+              <IconStyled>
+                <ClockIcon />
+              </IconStyled>
+              {descriptionLines[1]}
+            </LineWrapperStyled>
+          </DetailsStyled>
+        </DetailsWrapper>
+      );
+    }
+
+    return (
+      <DetailsWrapper>
         <DetailsStyled>
           <LineWrapperStyled>
-            <IconStyled>{icon}</IconStyled>
+            <IconStyled>
+              <CreditCardIcon />
+            </IconStyled>
             {description}
           </LineWrapperStyled>
         </DetailsStyled>
-      );
-    }
-    const description = periodMapper[period as Period]
-      ? `${t(
-          `period.${period}`,
-          periodMapper[period as Period].accessText as string
-        )} ${t('offer-checkout-card.season-pass', 'season pass')}`
-      : '';
+      </DetailsWrapper>
+    );
+  };
+
+  const renderDescriptionForPass = () => {
+    const generateDescriptionForPass = () => {
+      const icon = <CalendarIcon />;
+      if (!period) {
+        const date = dateFormat(expiresAt, true);
+        const description = t('pass-desc.date', `Access until {{date}}`, {
+          date
+        });
+        return { icon, description };
+      }
+      const description = periodMapper[period as Period]
+        ? `${t(
+            `period.${period}`,
+            periodMapper[period as Period].accessText as string
+          )} ${t('offer-checkout-card.season-pass', 'season pass')}`
+        : '';
+      return { icon, description };
+    };
+
+    const { icon, description } = generateDescriptionForPass();
+
     return (
       <DetailsStyled>
         <LineWrapperStyled>
-          <IconStyled>{icon}</IconStyled> {description}
+          <IconStyled>{icon}</IconStyled>
+          {description}
         </LineWrapperStyled>
       </DetailsStyled>
     );
-  }
-  if (offerType === 'E') {
+  };
+
+  const renderDescriptionForPayPerViewEvent = () => {
     const icon = <CalendarIcon />;
     const description = `Pay-per-view event ${
       startTime ? dateFormat(startTime, true) : ''
@@ -218,12 +244,14 @@ const OfferDetailsDescription = ({
     return (
       <DetailsStyled>
         <LineWrapperStyled>
-          <IconStyled>{icon}</IconStyled> {description}
+          <IconStyled>{icon}</IconStyled>
+          {description}
         </LineWrapperStyled>
       </DetailsStyled>
     );
-  }
-  if (offerType === 'R') {
+  };
+
+  const renderDescriptionForTVOD = () => {
     const icon = <CalendarIcon />;
     const description = periodMapper[period as Period]
       ? `${t(
@@ -234,12 +262,14 @@ const OfferDetailsDescription = ({
     return (
       <DetailsStyled>
         <LineWrapperStyled>
-          <IconStyled>{icon}</IconStyled> {description}
+          <IconStyled>{icon}</IconStyled>
+          {description}
         </LineWrapperStyled>
       </DetailsStyled>
     );
-  }
-  if (offerType === 'A') {
+  };
+
+  const renderDescriptionForUnlimitedAccess = () => {
     const icon = <TagIcon />;
     const description = t(
       'offer-checkout-card.unlimited-access',
@@ -248,12 +278,33 @@ const OfferDetailsDescription = ({
     return (
       <DetailsStyled>
         <LineWrapperStyled>
-          <IconStyled>{icon}</IconStyled> {description}
+          <IconStyled>{icon}</IconStyled>
+          {description}
         </LineWrapperStyled>
       </DetailsStyled>
     );
-  }
-  return <DetailsStyled />;
+  };
+
+  const renderDescriptionByType = () => {
+    if (offerType === 'S') {
+      return renderDescriptionForSubscription();
+    }
+    if (offerType === 'P') {
+      return renderDescriptionForPass();
+    }
+    if (offerType === 'E') {
+      return renderDescriptionForPayPerViewEvent();
+    }
+    if (offerType === 'R') {
+      return renderDescriptionForTVOD();
+    }
+    if (offerType === 'A') {
+      return renderDescriptionForUnlimitedAccess();
+    }
+    return false;
+  };
+
+  return renderDescriptionByType();
 };
 
 export default OfferDetailsDescription;
