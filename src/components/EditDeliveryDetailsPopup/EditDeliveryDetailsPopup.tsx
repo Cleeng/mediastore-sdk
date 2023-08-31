f7220c4519838464dc2d77d2de3c84bf399a8bc5
m4tewoosh <mnowak@cleeng.com>
2023-08-31 14:28:23 +0200

    refactor

--- src/components/EditDeliveryDetailsPopup/EditDeliveryDetailsPopup.tsx
+++ src/components/EditDeliveryDetailsPopup/EditDeliveryDetailsPopup.tsx
@@ -102,39 +102,37 @@ const EditDeliveryDetailsPopup = () => {
             {t('edit-delivery-details-popup.header', 'Edit Delivery Details')}
           </HeaderStyled>
           <InfoTextStyled>
-            {loading ? (
-              <>
-                <SkeletonWrapper height={32} margin="0 0 24px 0" />
-              </>
-            ) : (
-              <>
-                {isGiftEditable ? (
-                  <>
-                    {t(
-                      'edit-delivery-details-popup.info-text-1',
-                      'You are editing information for your'
-                    )}
-                    <p>{t(`offer-title-${offerId}`, offerTitle)}</p>
-                    {t(
-                      'edit-delivery-details-popup.info-text-2',
-                      'Please take a moment to update your gift delivery details.'
-                    )}
-                  </>
-                ) : (
-                  <>
-                    {isGiftSent
-                      ? t(
-                          'edit-delivery-details-popup.info-text-disabled',
-                          'It is not possible to edit delivery details as the gift has been already sent out to the recipient.'
-                        )
-                      : t(
-                          'edit-delivery-details-popup.info-text-disabled2',
-                          'It is not possible to edit delivery details as the gift will be sent to the recipient soon.'
-                        )}
-                  </>
-                )}
-              </>
-            )}
+            <SkeletonWrapper
+              height={32}
+              margin="0 0 24px 0"
+              showChildren={!loading}
+            >
+              {isGiftEditable ? (
+                <>
+                  {t(
+                    'edit-delivery-details-popup.info-text-1',
+                    'You are editing information for your'
+                  )}
+                  <p>{t(`offer-title-${offerId}`, offerTitle)}</p>
+                  {t(
+                    'edit-delivery-details-popup.info-text-2',
+                    'Please take a moment to update your gift delivery details.'
+                  )}
+                </>
+              ) : (
+                <>
+                  {isGiftSent
+                    ? t(
+                        'edit-delivery-details-popup.info-text-disabled',
+                        'It is not possible to edit delivery details as the gift has been already sent out to the recipient.'
+                      )
+                    : t(
+                        'edit-delivery-details-popup.info-text-disabled2',
+                        'It is not possible to edit delivery details as the gift will be sent to the recipient soon.'
+                      )}
+                </>
+              )}
+            </SkeletonWrapper>
           </InfoTextStyled>
           <RecipientForm isMyAccount />
           <ButtonsStyled>
