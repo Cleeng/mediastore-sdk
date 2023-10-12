7d668d62da7d05a0a0e2ebe59e82992f02d8cafb
m4tewoosh <mnowak@cleeng.com>
2023-10-12 14:48:38 +0200

    feat/MSSDK-1528: change ThankYou page copy

--- src/components/RedeemGift/ThankYouPage.tsx
+++ src/components/RedeemGift/ThankYouPage.tsx
@@ -5,7 +5,8 @@ import { LinkStyled } from 'components/ThankYouPage/ThankYouPageStyled';
 import {
   HeaderStyled,
   InfoTextStyled,
-  ThankYouPageStyled
+  ThankYouPageStyled,
+  InfoTextWrapper
 } from './ThankYouPage.styled';
 
 const ThankYouPage = () => {
@@ -24,17 +25,22 @@ const ThankYouPage = () => {
             'Your gift have been successfully redeemed.'
           )}
         </p>
-        <Trans i18nKey="redeem-gift.thank-you-page.manage-text2">
-          We hope you love it. If you need help from us with your account, you
-          can always find it
-          <LinkStyled
-            href={getData('CLEENG_MY_ACCOUNT_URL')}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            here.
-          </LinkStyled>
-        </Trans>
+        {t(
+          'redeem-gift.thank-you-page.payment-method-text',
+          'We hope you love it. Remember that in order to keep your access after your gift expires, you will need to add a payment method to your account.'
+        )}
+        <InfoTextWrapper>
+          <Trans i18nKey="redeem-gift.thank-you-page.manage-text2">
+            If you need help from us with your account, you can always find it
+            <LinkStyled
+              href={getData('CLEENG_MY_ACCOUNT_URL')}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here.
+            </LinkStyled>
+          </Trans>
+        </InfoTextWrapper>
       </InfoTextStyled>
     </ThankYouPageStyled>
   );
