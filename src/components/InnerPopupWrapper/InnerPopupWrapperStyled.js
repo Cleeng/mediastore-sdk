b3551715677dc1a8fec66c8486b353afbb492e7e
Paweł Kaczmarek <pkaczmarek@cleeng.com>
2023-10-20 09:14:34 +0200

    fix/MSSDK-988: Unset the problematic min-height

--- src/components/InnerPopupWrapper/InnerPopupWrapperStyled.js
+++ src/components/InnerPopupWrapper/InnerPopupWrapperStyled.js
@@ -10,7 +10,6 @@ import Card from 'components/Card';
 
 export const CardStyled = styled(Card)`
   border: 1px solid ${LineColor};
-  min-height: 500px;
   display: flex;
   padding: 0;
   margin: 0;
