ea5c2580572ea344e1c7dcb722b9b594ffd3f350
m4tewoosh <mnowak@cleeng.com>
2024-02-13 13:19:34 +0100

    feat/MSSDK-1388: remove moved logic from OfferCheckoutCard

--- src/components/OfferCheckoutCard/OfferCheckoutCard.tsx
+++ src/components/OfferCheckoutCard/OfferCheckoutCard.tsx
@@ -3,19 +3,11 @@ import SubscriptionIcon from 'components/SubscriptionIcon';
 import SkeletonWrapper from 'components/SkeletonWrapper';
 import { useAppSelector } from 'redux/store';
 import formatNumber from 'util/formatNumber';
-import {
-  currencyFormat,
-  dateFormat,
-  periodMapper,
-  Period
-} from 'util/planHelper';
+import { currencyFormat } from 'util/planHelper';
 import { selectOffer, selectOnlyOffer } from 'redux/offerSlice';
-import { selectGift } from 'redux/giftSlice';
 import { selectOnlyOrder } from 'redux/orderSlice';
-import { selectDeliveryDetails } from 'redux/deliveryDetailsSlice';
 import calculateGrossPriceForFreeOffer from 'util/calculateGrossPriceForFreeOffer';
 import OfferDescription from 'components/OfferCheckoutCard/OfferDescription';
-import getReadablePeriod from './OfferCheckoutCard.utils';
 import {
   WrapperStyled,
   InnerWrapper,
@@ -52,10 +44,6 @@ const OfferCheckoutCard = ({
   } = useAppSelector(selectOffer);
 
   const {
-    verifiedGift: { redeemMode, redeemRefusalReason }
-  } = useAppSelector(selectGift);
-
-  const {
     priceBreakdown: { offerPrice },
     taxRate,
     country,
@@ -67,8 +55,6 @@ const OfferCheckoutCard = ({
       periods: discountedPeriods
     }
   } = useAppSelector(selectOnlyOrder);
-
-  const { isGift } = useAppSelector(selectDeliveryDetails);
 
   const offerType = offerId?.charAt(0);
   const currencySymbol = currencyFormat[currency];
@@ -93,157 +79,6 @@ const OfferCheckoutCard = ({
       ? t(`offer-price.period-${period}`, period)
       : null;
 
-  const generateTrialDescription = () => {
-    if (period === 'season' && freeDays) {
-      const formattedDescription = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) after {{freeDays}} days and will be renewed on the next season start date.`;
-      return t(
-        `subscription-desc.trial-days.period-season`,
-        formattedDescription,
-        {
-          currencySymbol,
-          grossPrice,
-          taxCopy,
-          freeDays
-        }
-      );
-    }
-
-    if (freeDays) {
-      const description = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) after {{freeDays}} days. </br> Next payments will occur every ${getReadablePeriod(
-        period
-      )}.`;
-      return t(`subscription-desc.trial-days.period-${period}`, description, {
-        currencySymbol,
-        grossPrice,
-        taxCopy,
-        freeDays
-      });
-    }
-
-    // freePeriods
-    let formattedDescription =
-      'You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) ';
-    if (period === 'month') {
-      formattedDescription +=
-        freePeriods === 1
-          ? 'after month. </br>Next payments will occur every month.'
-          : 'after {{freePeriods}} months. </br>Next payments will occur every month.';
-    } else if (period === 'week') {
-      formattedDescription +=
-        freePeriods === 1
-          ? 'after week. </br>Next payments will occur every week.'
-          : 'after {{freePeriods}} weeks. </br>Next payments will occur every week.';
-    }
-    return t(
-      `subscription-desc.trial-period${
-        freePeriods === 1 ? '' : 's'
-      }.period-${period}`,
-      formattedDescription,
-      {
-        currencySymbol,
-        grossPrice,
-        taxCopy,
-        freePeriods
-      }
-    );
-  };
-
-  const generateCouponDescription = () => {
-    const formattedTotalPrice = formatNumber(totalPrice);
-    const regularPrice =
-      period === 'season'
-        ? formatNumber(customerPriceInclTax)
-        : calculateGrossPriceForFreeOffer(
-            offerPrice,
-            taxRate,
-            customerPriceInclTax
-          );
-
-    return t(
-      `subscription-desc-coupon`,
-      `You will be charged ${currencySymbol}${formattedTotalPrice} (incl. ${taxCopy}) per ${period} for the promotional period.<br/>After the promotion ends, you will be charged ${currencySymbol}${regularPrice} per ${period}.`,
-      {
-        currencySymbol,
-        formattedTotalPrice,
-        taxCopy,
-        period,
-        discountedPeriods,
-        regularPrice
-      }
-    );
-  };
-
-  const generateSubscriptionDescription = () => {
-    if (discountType === 'coupon') {
-      return generateCouponDescription();
-    }
-
-    if (isGift) {
-      const description = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}).`;
-      return t(`subscription-desc.gift`, description, {
-        currencySymbol,
-        grossPrice,
-        taxCopy
-      });
-    }
-
-    if (!isTrialAvailable) {
-      if (period === 'season') {
-        const description = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) and will be renewed on the next season start date.`;
-        return t(`subscription-desc.period-season`, description, {
-          currencySymbol,
-          grossPrice,
-          taxCopy
-        });
-      }
-      const formattedDescription = `You will be charged {{currencySymbol}}{{grossPrice}} (incl. {{taxCopy}}) every ${getReadablePeriod(
-        period
-      )}.`;
-      return t(`subscription-desc.period-${period}`, formattedDescription, {
-        currencySymbol,
-        grossPrice,
-        taxCopy
-      });
-    }
-
-    return generateTrialDescription();
-  };
-
-  const renderDescription = () => {
-    if (offerType === 'S') {
-      return generateSubscriptionDescription();
-    }
-    if (offerType === 'P') {
-      if (!period) {
-        const date = dateFormat(expiresAt, true);
-        return t('pass-desc.date', `Access until {{date}}`, { date });
-      }
-      return periodMapper[period as Period]
-        ? `${t(
-            `period.${period}`,
-            periodMapper[period as Period].accessText as string
-          )} ${t('offer-checkout-card.season-pass', 'season pass')}`
-        : '';
-    }
-    if (offerType === 'E') {
-      return `Pay-per-view event ${
-        startTime ? dateFormat(startTime, true) : ''
-      }`;
-    }
-    if (offerType === 'R') {
-      return periodMapper[period as Period]
-        ? `${t(
-            `period.${period}`,
-            periodMapper[period as Period].accessText as string
-          )} ${t('offer-checkout-card.access', 'access')}`
-        : '';
-    }
-    if (offerType === 'A') {
-      return t('offer-checkout-card.unlimited-access', 'Unlimited access');
-    }
-    return '';
-  };
-
   const renderTrialBadgeDescription = () => {
     if (freeDays) {
       return t('trial-badge-days', `{{freeDays}} days free trial`, {
@@ -264,64 +99,6 @@ const OfferCheckoutCard = ({
     );
   };
 
-  const getRedeemGiftDescription = () => {
-    if (redeemMode === 'EXTEND') {
-      return t(
-        `redeem-gift.description.existing-subscription.period-${period}`,
-        `Your existing subscription will be extended for additional ${getReadablePeriod(
-          period
-        )} for free.`
-      );
-    }
-
-    switch (redeemRefusalReason) {
-      case 'EXTERNALLY_MANAGED_SUBSCRIPTION':
-        return t(
-          'redeem-gift.description.refusal.external',
-          'You already have an active subscription in a different store, perhaps through a mobile app. Once that subscription expires, you can come back and redeem your gift code here.'
-        );
-
-      case 'REDEEMED':
-        return t(
-          'redeem-gift.description.refusal.redeemed',
-          'This gift code has already been used.'
-        );
-
-      case 'INACTIVE_OFFER':
-        return t(
-          'redeem-gift.description.refusal.offer-inactive',
-          'Sorry, but the offer linked to your gift code is no longer available.'
-        );
-
-      case 'OFFER_GEO_RESTRICTED':
-        return t(
-          'redeem-gift.description.refusal.offer-georestricted',
-          "Sorry, this gift code can't be used in your current location due to geographic restrictions."
-        );
-
-      case 'RECURRING_PROCESS_ALREADY_STARTED':
-        return t(
-          'redeem-gift.description.refusal.recurring-process-started',
-          "You’ll be able to redeem your gift code once we've finished processing a payment on your existing subscription"
-        );
-
-      case 'MORE_THAN_ONE_MATCHING_SUBSCRIPTION':
-        return t(
-          'redeem-gift.description.refusal.more-than-one-matching-subscription',
-          'You currently have multiple subscriptions granting you access to the content this gift provides. When one of those subscriptions expires, you can come back and redeem your gift code here.'
-        );
-
-      case 'PUBLISHER_GEO_RESTRICTED':
-        return t(
-          'redeem-gift.description.refusal.publisher-georestricted',
-          "Sorry, this gift code can't be used in your current location due to geographic restrictions."
-        );
-
-      default:
-        return '';
-    }
-  };
-
   return (
     <WrapperStyled>
       <SkeletonWrapper showChildren={!loading} width={50} height={50}>
@@ -336,7 +113,6 @@ const OfferCheckoutCard = ({
           <TitleStyled>
             {t(`offer-title-${offerId}`, title || offerV2Title)}
           </TitleStyled>
-          {/* The name PublisherDescriptionStyled is valid only here */}
           {publisherDescription && (
             <PublisherDescriptionStyled>
               {t(`offer-description-${offerId}`, publisherDescription)}
@@ -348,17 +124,9 @@ const OfferCheckoutCard = ({
           width={300}
           margin="0 0 10px 10px"
         >
-          {/* The name PublisherDescriptionStyled isn't accurate here - it's left only not to duplicate styled component. Ultimately, OfferDescription should be used as seen in the below piee of code. */}
-          <PublisherDescriptionStyled
-            dangerouslySetInnerHTML={{
-              __html: isRedeemGift
-                ? getRedeemGiftDescription()
-                : renderDescription()
-            }}
-          />
           {/* The idea was to move all of the description logic to OfferDescription component. */}
           {/* It's worth mentioning that the logic inside OfferDescription does not include latest changes related to copy, neither those responsible for gifting descriptions */}
-          {/* <OfferDescription
+          <OfferDescription
             period={period}
             freeDays={freeDays}
             currencySymbol={currencySymbol}
@@ -375,7 +143,8 @@ const OfferCheckoutCard = ({
             offerType={offerType}
             startTime={startTime}
             expiresAt={expiresAt}
-          /> */}
+            isRedeemGift={isRedeemGift}
+          />
         </SkeletonWrapper>
       </InnerWrapper>
       {!isRedeemGift && (
