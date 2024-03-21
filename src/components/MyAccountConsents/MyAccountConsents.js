d3ce675b357fe29cdc71637061d528374be2e74e
m4tewoosh <mnowak@cleeng.com>
2024-03-21 14:24:47 +0100

    feat/MSSDK-1444: add consents translations, remove CreditCardExample component

--- src/components/MyAccountConsents/MyAccountConsents.js
+++ src/components/MyAccountConsents/MyAccountConsents.js
@@ -132,7 +132,10 @@ class MyAccountConsents extends Component {
                     }
                     width="100%"
                   >
-                    {t('Update Terms')}
+                    {t(
+                      'myaccount-consents.button.update-terms',
+                      'Update Terms'
+                    )}
                   </ButtonStyled>
                 ) : (
                   <>
@@ -145,7 +148,7 @@ class MyAccountConsents extends Component {
                         })
                       }
                     >
-                      {t('Cancel')}
+                      {t('myaccount-consents.button.cancel', 'Cancel')}
                     </ButtonStyled>
                     <ButtonStyled
                       theme="confirm"
@@ -155,7 +158,7 @@ class MyAccountConsents extends Component {
                       {(isSubmittingPending && (
                         <Loader buttonLoader color="#ffffff" />
                       )) ||
-                        t('Save')}
+                        t('myaccount-consents.button.save', 'Save')}
                     </ButtonStyled>
                   </>
                 )}
--- src/components/Popup/Popup.const.js
+++ src/components/Popup/Popup.const.js
@@ -7,7 +7,13 @@ const popupData = {
         title: 'Hi there!',
         text:
           'Your brand new account is waiting for you. Just one last step. We would like you to review our terms and conditions before continuing to your account.',
-        buttonText: 'Let’s do it'
+        buttonText: 'Let’s do it',
+        translationKeys: {
+          header: 'popup.not-checked-terms.header',
+          title: 'popup.not-checked-terms.title',
+          text: 'popup.not-checked-terms.text',
+          button: 'popup.not-checked-terms.button'
+        }
       },
       {
         headerTitle: 'Terms & Conditions',
@@ -15,7 +21,13 @@ const popupData = {
         title: 'Terms & Conditions',
         text:
           'Please accept our terms and conditions before continuing to your account.',
-        buttonText: 'Continue'
+        buttonText: 'Continue',
+        translationKeys: {
+          header: 'popup.not-checked-terms2.header',
+          title: 'popup.not-checked-terms2.title',
+          text: 'popup.not-checked-terms2.text',
+          button: 'popup.not-checked-terms2.button'
+        }
       }
     ]
   },
@@ -27,7 +39,13 @@ const popupData = {
         title: 'A change in our terms',
         text:
           'One of our mandatory terms has been updated. Review the changes to continue.',
-        buttonText: 'Okey, Go next'
+        buttonText: 'Okey, Go next',
+        translationKeys: {
+          header: 'popup.terms-update-required.header',
+          title: 'popup.terms-update-required.title',
+          text: 'popup.terms-update-required.text',
+          button: 'popup.terms-update-required.button'
+        }
       },
       {
         headerTitle: 'Terms update',
@@ -35,7 +53,13 @@ const popupData = {
         title: 'Update to terms & conditions',
         text:
           'Please review and accept our updated terms and conditions before continuing to your account.',
-        buttonText: 'Continue'
+        buttonText: 'Continue',
+        translationKeys: {
+          header: 'popup.terms-update-required2.header',
+          title: 'popup.terms-update-required2.title',
+          text: 'popup.terms-update-required2.text',
+          button: 'popup.terms-update-required2.button'
+        }
       }
     ]
   },
@@ -45,8 +69,15 @@ const popupData = {
         headerTitle: 'Terms update',
         icon: true,
         title: 'Update to terms & conditions',
-        text: `We have updated our terms and conditions. Please take a few minutes to review the changes in your profile section.`,
-        buttonText: 'Continue'
+        text:
+          'We have updated our terms and conditions. Please take a few minutes to review the changes in your profile section',
+        buttonText: 'Continue',
+        translationKeys: {
+          header: 'popup.consents-update-required.header',
+          title: 'popup.consents-update-required.title',
+          text: 'popup.consents-update-required.text',
+          button: 'popup.consents-update-required.button'
+        }
       }
     ]
   },
@@ -58,14 +89,28 @@ const popupData = {
         title: 'A change in our terms',
         text: `One of our mandatory terms has been updated. We’ve also updated our Consents Details which you can find in Update Profile tab.`,
         secondText: 'Review the changes to continue.',
-        buttonText: 'Okey, Go next!'
+        buttonText: 'Okey, Go next!',
+        translationKeys: {
+          header: 'popup.complex-update.header',
+          title: 'popup.complex-update.title',
+          text: 'popup.complex-update.text',
+          secondText: 'popup.complex-update.secondText',
+          button: 'popup.complex-update.button'
+        }
       },
       {
         headerTitle: 'Terms update',
         icon: null,
         title: 'Update to terms & conditions',
-        text: `Please review and accept our updated terms and conditions before continuing to your account.`,
-        buttonText: 'Continue'
+        text:
+          'Please review and accept our updated terms and conditions before continuing to your account.',
+        buttonText: 'Continue',
+        translationKeys: {
+          header: 'popup.complex-update2.header',
+          title: 'popup.complex-update2.title',
+          text: 'popup.complex-update2.text',
+          button: 'popup.complex-update2.button'
+        }
       }
     ]
   }
