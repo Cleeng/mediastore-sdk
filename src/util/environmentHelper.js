38fe56d95195deb769e61b5ec9e81a4cb474f1f8
marcinkolos <56196429+marcinkolos@users.noreply.github.com>
2024-05-20 11:12:34 +0200

    cleanup staging2

--- src/util/environmentHelper.js
+++ src/util/environmentHelper.js
@@ -3,7 +3,6 @@ import { getData } from 'util/appConfigHelper';
 const ENVIRONMENTS = {
   DEVELOPMENT: '',
   STAGING: 'https://hc0f1jaa70.execute-api.eu-west-1.amazonaws.com/staging',
-  STAGING2: 'https://hc0f1jaa70.execute-api.eu-west-1.amazonaws.com/staging',
   SANDBOX: 'https://mediastoreapi-sandbox.cleeng.com',
   PRODUCTION: 'https://mediastoreapi.cleeng.com'
 };
@@ -15,8 +14,6 @@ const getApiURL = () => {
       return ENVIRONMENTS.DEVELOPMENT;
     case 'staging':
       return ENVIRONMENTS.STAGING;
-    case 'staging2':
-      return ENVIRONMENTS.STAGING2;
     case 'sandbox':
       return ENVIRONMENTS.SANDBOX;
     case 'production':
