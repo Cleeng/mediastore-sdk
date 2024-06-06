61eba7d43d429a152738718da3dc128abc06c5f3
Aleksander Sadaj <asadaj@cleeng.com>
2024-06-06 08:35:30 +0200

    feat/MSSDK-1709: swap vite react plugin to SWC to gain performance during builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import path from 'node:path';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
