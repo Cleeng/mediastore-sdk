757a1f067e189515957e9ebf374e7bc0dcdd13e1
Paweł Kaczmarek <pkaczmarek@cleeng.com>
2024-06-28 10:12:14 +0200

    feat/MSSDK-1738: Add Google reCAPTCHA for testing purposes

--- src/api/Payment/submitPaymentWithoutDetails.js
+++ src/api/Payment/submitPaymentWithoutDetails.js
@@ -2,14 +2,14 @@ import { getData } from 'util/appConfigHelper';
 import fetchWithJWT from 'util/fetchHelper';
 import getApiURL from 'util/environmentHelper';
 
-const submitPaymentWithoutDetails = async () => {
+const submitPaymentWithoutDetails = async (captchaValue) => {
   const API_URL = getApiURL();
   const orderId = parseInt(getData('CLEENG_ORDER_ID') || '0', 10);
   const url = `${API_URL}/payments`;
 
   return fetchWithJWT(url, {
     method: 'POST',
-    body: JSON.stringify({ orderId })
+    body: JSON.stringify({ orderId, captchaValue })
   })
     .then(async (res) => {
       const { responseData, errors } = await res.json();
