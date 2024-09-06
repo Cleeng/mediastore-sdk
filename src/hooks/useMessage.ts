4aab66dfb63b51a057c44d0f84b5cc07f6855d0c
m4tewoosh <mnowak@cleeng.com>
2024-09-06 15:06:24 +0200

    feat/MSSDK-1887: rename file

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Message, MessageType, UseMessageReturnType } from 'types/hooks';
+import { Message, MessageType, UseMessageReturnType } from 'types/hooks.types';
 
 const useMessage = (): UseMessageReturnType => {
   const [message, setMessageText] = useState('');
