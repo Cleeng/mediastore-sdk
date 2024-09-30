31cb3ef11a0515f579b007e480c23626ff375cfa
Aleksander Sadaj <asadaj@cleeng.com>
2024-09-30 12:06:32 +0200

    feat/MSSDK-1925: temp commit for testing

--- src/package.ts
+++ src/package.ts
@@ -47,9 +47,7 @@ if (true) {
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
     environment: cleengEnvironment,
-    release: `@cleeng/mediastore-sdk@${
-      import.meta.env.VITE_MEDIASTORE_SDK_VERSION
-    }`,
+    release: `@cleeng/mediastore-sdk@${process.env.npm_package_version}`,
     integrations: [
       Sentry.browserTracingIntegration(),
       Sentry.replayIntegration()
