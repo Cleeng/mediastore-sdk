248036cff10f8df2c2d6c70304376cac44606d2b
Aleksander Sadaj <asadaj@cleeng.com>
2024-10-01 11:11:18 +0200

    feat/MSSDK-1925: sourcemaps testing

--- src/components/RegisterPage/Register.tsx
+++ src/components/RegisterPage/Register.tsx
@@ -6,24 +6,24 @@ import Button from 'components/Button';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import { useTranslation } from 'react-i18next';
+import { useCallback } from 'react';
 import RegisterForm from './RegisterForm';
 import { RegisterProps } from './Register.types';
 
 const Register = ({ onSuccess, onHaveAccountClick }: RegisterProps) => {
   const { t } = useTranslation();
 
+  const handleLinkClick = useCallback(() => {
+    throw new Error('this is thrown from a callback');
+  }, []);
+
   return (
     <RegisterWrapperStyled>
       <Header />
       <ContentWrapperStyled>
         <RegisterForm onSuccess={onSuccess} />
-        <a
-          id='crashingLinkaaaaaaaaaaaaa'
-          onClick={() => {
-            throw new Error('please just start working properly');
-          }}
-        >
-          make a wish (that will not work)
+        <a id='crashingLinkaaaaaaaaaaaaabbbbbbbbbbb' onClick={handleLinkClick}>
+          make a wish (that will not work aaaaaaaa)
         </a>
         <Button
           theme='secondary'
