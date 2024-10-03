e869f9354442453630d91e0011f94c485c4b240e
m4tewoosh <mnowak@cleeng.com>
2024-10-03 09:59:26 +0200

    feat/MSSDK-1934: add button default options

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -14,30 +14,28 @@ const Button = ({
   padding,
   size,
   testid,
-  variant,
-  type,
+  variant = 'primary',
+  type = 'button',
   width
-}: ButtonProps) => {
-  return (
-    <ButtonStyled
-      aria-label={label}
-      className={className}
-      data-testid={testid}
-      disabled={disabled}
-      onClick={onClickFn}
-      type={type}
-      $fontSize={fontSize}
-      $fontWeight={fontWeight}
-      $icon={icon}
-      $margin={margin}
-      $padding={padding}
-      $size={size}
-      $variant={variant}
-      $width={width}
-    >
-      {children}
-    </ButtonStyled>
-  );
-};
+}: ButtonProps) => (
+  <ButtonStyled
+    aria-label={label}
+    className={className}
+    data-testid={testid}
+    disabled={disabled}
+    onClick={onClickFn}
+    type={type}
+    $fontSize={fontSize}
+    $fontWeight={fontWeight}
+    $icon={icon}
+    $margin={margin}
+    $padding={padding}
+    $size={size}
+    $variant={variant}
+    $width={width}
+  >
+    {children}
+  </ButtonStyled>
+);
 
 export default Button;
