818feca032ccf07574a755597e9371db2531be38
Aleksander Sadaj <asadaj@cleeng.com>
2024-10-11 15:24:24 +0200

    feat/MSSDK-1948: Adjust Sentry integration for shared environments capability

--- src/sentryInit.ts
+++ src/sentryInit.ts
@@ -10,6 +10,19 @@ import { getData } from 'util/appConfigHelper';
 const SENTRY_SUPPORTED_ENVIRONMENTS = ['production', 'sandbox'];
 const cleengEnvironment = getData('CLEENG_ENVIRONMENT');
 
+console.log(
+  '########## mssdk sentry init file root',
+  SENTRY_SUPPORTED_ENVIRONMENTS
+);
+console.log(
+  '########## mssdk sentry init file root cleeng env',
+  cleengEnvironment
+);
+console.log(
+  '########## mssdk sentry init file root result:',
+  SENTRY_SUPPORTED_ENVIRONMENTS.includes(cleengEnvironment)
+);
+
 if (SENTRY_SUPPORTED_ENVIRONMENTS.includes(cleengEnvironment)) {
   const mediastoreSDKRegexp = /@?cleeng.mediastore-sdk/;
 
