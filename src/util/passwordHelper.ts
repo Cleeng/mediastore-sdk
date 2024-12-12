36a980c358efcd16eea304cb78f08a9ceed6f19a
m4tewoosh <mnowak@cleeng.com>
2024-12-12 18:02:19 +0100

    feat/MSSDK-2032: add regex comments, add lookahead to eliminate backtracking

--- src/util/passwordHelper.ts
+++ src/util/passwordHelper.ts
@@ -9,15 +9,16 @@ const getPasswordStrength = (password: string) => {
   }
 
   const criteria = [
-    { regex: /[a-z]/, score: 1 },
-    { regex: /[A-Z]/, score: 5 },
-    { regex: /\d/, score: 5 },
-    { regex: /\d.*\d/, score: 5 },
-    { regex: /[!,@#$%^&*?_~]/, score: 5 },
-    { regex: /[!,@#$%^&*?_~].*[!,@#$%^&*?_~]/, score: 5 },
-    { regex: /[a-z].*[A-Z]|[A-Z].*[a-z]/, score: 2 },
-    { regex: /\d.*\D|\D.*\d/, score: 2 },
-    { regex: /[a-z].*[A-Z].*\d.*[!,@#$%^&*?_~]/, score: 2 }
+    // checks if there is at least:
+    { regex: /[a-z]/, score: 1 }, // one lowercase letter
+    { regex: /[A-Z]/, score: 5 }, // one uppercase letter
+    { regex: /\d/, score: 5 }, // one digit
+    { regex: /(?=(.*\d){2})/, score: 5 }, // two digits
+    { regex: /[!,@#$%^&*?_~]/, score: 5 }, // one special character
+    { regex: /(?=(.*[!,@#$%^&*?_~]){2})/, score: 5 }, // two special characters
+    { regex: /(?=.*[a-z])(?=.*[A-Z])/, score: 2 }, // one lowercase and one uppercase letter
+    { regex: /(?=.*\d)(?=.*\D)/, score: 2 }, // digit and one non-digit character
+    { regex: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!,@#$%^&*?_~])/, score: 2 } // one lowercase letter, one uppercase letter, one digit, and one special character
   ];
 
   const score = criteria.reduce((sum, criterion) => {
