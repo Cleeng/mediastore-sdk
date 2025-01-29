632d537d49b6580172a211d048f8ad49edd2e250
Maksymilian Tarnoruda <m.tarnoruda@gmail.com>
2025-01-29 09:40:02 +0100

    add dev env

--- src/util/environmentHelper.js
+++ src/util/environmentHelper.js
@@ -1,7 +1,7 @@
 import { getData } from 'util/appConfigHelper';
 
 const ENVIRONMENTS = {
-  DEVELOPMENT: '',
+  DEVELOPMENT: 'https://0ndqcbli79.execute-api.eu-west-1.amazonaws.com/dev',
   STAGING: 'https://hc0f1jaa70.execute-api.eu-west-1.amazonaws.com/staging',
   SANDBOX: 'https://mediastoreapi-sandbox.cleeng.com',
   PRODUCTION: 'https://mediastoreapi.cleeng.com'
