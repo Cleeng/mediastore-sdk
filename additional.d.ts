2c1134c5b3039d60aac4f342121ff9ac032ab5c2
Paweł Kaczmarek <pkaczmarek@cleeng.com>
2025-02-24 10:21:40 +0100

    feat/MSSDK-2072: Add dts library to provide types with the package

--- additional.d.ts
+++ additional.d.ts
@@ -1,1 +1,9 @@
-declare module '*.svg';
+declare module '*.svg?inline' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
