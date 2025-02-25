21fec0b1f95ccddf4692817e4fdcab551f51cfaa
Aleksander Sadaj <asadaj@cleeng.com>
2025-02-25 10:44:00 +0100

    feat/MSSDK-1993: Google Recaptcha integration (#448)
    * Feat/MSSDK-2048: Add Google reCAPTCHA into Register flow (#443)
    
    * feat/MSSDK-2048: Add google recaptcha into Register flow
    
    * feat/MSSDK-2048: Changes after CR
    
    * [Feat/MSSDK-2050]: Add configuration method (#444)
    
    * feat/MSSDK-2048: Add google recaptcha into Register flow
    
    * feat/MSSDK-2048: Changes after CR
    
    * feat/MSSDK-2050: Add configuration method for Google reCAPTCHA
    
    * feat/MSSDK-2050: Fix sonarcube error
    
    * feat/MSSDK-2050: Remove logging
    
    * fix/MSSDK-2050: fix the ReCaptcha not being triggered (#445)
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * chore/MSSDK-2050: add debugging logs for recaptcha in register form
    
    * fix/MSSDK-2050: commit cleaned up code with the final fix
    
    * chore/MSSDK-2050: add debugging logs
    
    * chore/MSSDK-2050: add debugging logs
    
    * chore/MSSDK-2050: add debugging logs
    
    * chore/MSSDK-2050: add debugging logs
    
    * chore/MSSDK-2050: add debugging logs
    
    * chore/MSSDK-2050: add debugging logs
    
    * chore/MSSDK-2050: add debugging logs
    
    * chore/MSSDK-2050: clean up code for code review
    
    * chore/MSSDK-2050: recreate pnpm-lock
    
    * chore/MSSDK-2050: remove the isolated prop - needs confirmation if we would like to use this
    
    * feat/MSSDK-2049: captcha challenge in purchase flow (#447)
    
    * feat/MSSDK-2049: captcha challenge in purchase flow
    
    * feat/MSSDK-2049: add missing prepare script
    
    * feat/MSSDK-2049: fix deprecated husky command
    
    * feat/MSSDK-2049: fix deprecated husky command
    
    * feat/MSSDK-2049: add unit tests for the new recaptcha hook
    
    * feat/MSSDK-2112: Add errors for google recaptcha (#452)
    
    * feat/MSSDK-2112: Add errors for google recaptcha
    
    * feat/MSSDK-2112: Update after CR
    
    * fix/MSSDK-2112: Fix behaviour if challange called (#453)
    
    * fix/MSSDK-2133: Fix invalid token error nad small refactor (#459)
    
    * [chore/MSSDK-2051]: Update readme with info about Google reCAPTCHA (#468)
    
    * chore/MSSDK-2051: Update readme with info about Google reCAPTCHA config method
    
    * chore/MSSDK-2051: Update link to docs
    
    * chore/MSSDK-2051: Update readme with optional values
    
    * feat/MSSDK-1993: empty commit to bump GH actions
    
    ---------
    
    Co-authored-by: Paweł Kaczmarek <pkaczmarek@cleeng.com>

---
+++ src/components/RegisterPage/useRegisterForm.ts
@@ -0,0 +1,239 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import submitConsents from 'api/Customer/submitConsents';
+import {
+  validateRegisterPassword,
+  validateEmailField,
+  validateConsentsField
+} from 'util/validators';
+import { selectPublisherConfig } from 'appRedux/publisherConfigSlice';
+import { selectPublisherConsents } from 'appRedux/publisherConsentsSlice';
+import registerCustomer from 'api/Auth/registerCustomer';
+import getCustomerLocales from 'api/Customer/getCustomerLocales';
+import Auth from 'services/auth';
+import { useAppSelector } from 'appRedux/store';
+import { Consent as ConsentType } from 'types/Consents.types';
+import ERROR_CODES from 'util/errorCodes';
+import useCaptchaVerification from 'hooks/useCaptchaVerification';
+
+type Errors = {
+  email: string;
+  password: string;
+  consents: string;
+  captcha: string;
+};
+
+const errorsInitialState = {
+  email: '',
+  password: '',
+  consents: '',
+  captcha: ''
+};
+
+type UseRegisterFormProps = {
+  onSuccess: () => void;
+};
+
+function useRegisterForm({ onSuccess }: UseRegisterFormProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [consents, setConsents] = useState<boolean[]>([]);
+  const [errors, setErrors] = useState<Errors>(errorsInitialState);
+  const [generalError, setGeneralError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [consentDefinitions, setConsentDefinitions] = useState<ConsentType[]>(
+    []
+  );
+  const [processing, setProcessing] = useState(false);
+  const {
+    recaptchaRef,
+    showCaptchaOnRegister,
+    getCaptchaToken,
+    validateCaptchaToken
+  } = useCaptchaVerification();
+
+  const { t } = useTranslation();
+  const { publisherId, googleRecaptcha } = useAppSelector(
+    selectPublisherConfig
+  );
+  const { error: publisherConsentsError } = useAppSelector(
+    selectPublisherConsents
+  );
+
+  const handleClickShowPassword = () =>
+    setShowPassword((prevValue) => !prevValue);
+
+  const validateEmail = () => {
+    const message = validateEmailField(email);
+    setErrors((prevValue) => {
+      return {
+        ...prevValue,
+        email: message
+      };
+    });
+  };
+
+  const validatePassword = () => {
+    const message = validateRegisterPassword(password);
+    setErrors((prevValue) => {
+      return {
+        ...prevValue,
+        password: message
+      };
+    });
+  };
+
+  const validateFields = (captchaValue: string) => {
+    const errorFields = {
+      email: validateEmailField(email),
+      password: validateRegisterPassword(password),
+      consents: validateConsentsField(consents, consentDefinitions),
+      captcha: showCaptchaOnRegister ? validateCaptchaToken(captchaValue) : ''
+    };
+    setErrors(errorFields);
+    return !Object.values(errorFields).some((error) => error !== '');
+  };
+
+  const handleConsentsChange = (
+    value: boolean[],
+    consentDefinitionsParam: ConsentType[]
+  ) => {
+    setConsents(value);
+    setConsentDefinitions(consentDefinitionsParam);
+    setErrors((prevValue) => {
+      return {
+        ...prevValue,
+        consents: ''
+      };
+    });
+  };
+
+  const handlePasswordChange = (value: string) => {
+    setPassword(value);
+    setErrors((prevValue) => {
+      return {
+        ...prevValue,
+        password: ''
+      };
+    });
+  };
+
+  const handleRecaptchaChange = () => {
+    setErrors((prevValue) => {
+      return {
+        ...prevValue,
+        captcha: ''
+      };
+    });
+  };
+
+  const renderError = (message: string) => {
+    setProcessing(false);
+    if (showCaptchaOnRegister) recaptchaRef?.current?.reset();
+    setGeneralError(
+      message || t('register-form.error.general', 'An error occurred.')
+    );
+  };
+
+  const register = async (captchaValue = ''): Promise<boolean> => {
+    setProcessing(true);
+    const localesResponse = await getCustomerLocales();
+    if (!localesResponse.responseData) {
+      setProcessing(false);
+      renderError(t('register-form.error.general', 'An error occurred.'));
+      return false;
+    }
+    const { locale, country, currency } = localesResponse.responseData;
+
+    const response = await registerCustomer({
+      email,
+      password,
+      publisherId,
+      locale,
+      country,
+      currency,
+      captchaValue
+    });
+
+    if (!response.code) {
+      Auth.login(
+        false,
+        true,
+        email,
+        response?.responseData?.jwt,
+        response?.responseData?.refreshToken,
+        submitConsents,
+        [consents, consentDefinitions],
+        onSuccess
+      );
+      return true;
+    }
+
+    switch (response.code) {
+      case ERROR_CODES.USER.ALREADY_EXISTS:
+        renderError(
+          t('register-form.error.customer-exists', 'Customer already exists.')
+        );
+        break;
+      case ERROR_CODES.REQUEST.INVALID_REQUEST_BODY:
+        if (response.errors[0] === 'Email verification failed.') {
+          renderError(
+            t(
+              'register-form.error.email-verification-failed',
+              "We couldn't verify the email address you entered. Please check it for accuracy and try again. If you're sure the address is correct and still see this message, you may need to use a different email or contact support for help."
+            )
+          );
+        }
+        break;
+      case ERROR_CODES.CAPTCHA.VERIFICATION_FAILED:
+        renderError(
+          t(
+            'register-form.error.captcha-verification-failed',
+            'An error occurred during registration. Please try again later. If the issue persists, please reach out to our support team for assistance.'
+          )
+        );
+        break;
+      default:
+        renderError(t('register-form.error.general', 'An error occurred.'));
+        break;
+    }
+
+    return false;
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    let captchaToken = '';
+
+    if (showCaptchaOnRegister) {
+      const { captchaToken: fetchedCaptchaToken } = await getCaptchaToken();
+      captchaToken = fetchedCaptchaToken;
+    }
+
+    if (validateFields(captchaToken)) {
+      register(captchaToken);
+    }
+  };
+
+  return {
+    handleClickShowPassword,
+    validateEmail,
+    validatePassword,
+    handleConsentsChange,
+    handlePasswordChange,
+    handleRecaptchaChange,
+    handleSubmit,
+    setEmail,
+    errors,
+    generalError,
+    showPassword,
+    processing,
+    publisherConsentsError,
+    email,
+    password,
+    recaptchaRef,
+    googleRecaptcha
+  };
+}
+
+export default useRegisterForm;
