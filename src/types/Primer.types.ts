b1733993be505a82195a63322e07373acecb5261
m4tewoosh <mnowak@cleeng.com>
2025-03-17 13:53:00 +0100

    feat/MSSDK-2179: change Primer onSubmit type

--- src/types/Primer.types.ts
+++ src/types/Primer.types.ts
@@ -1,11 +1,11 @@
 export type PrimerProps = {
-  onSubmit: (id?: string) => void;
+  onSubmit: (id?: string) => void | Promise<void>;
   selectPaymentMethod: (paymentMethod: string) => void;
   isMyAccount?: boolean;
 };
 
 export type UsePrimerHookProps = {
   selectPaymentMethod: (paymentMethod: string) => void;
-  onSubmit: (id?: string) => void;
+  onSubmit: (id?: string) => void | Promise<void>;
   isMyAccount?: boolean;
 };
