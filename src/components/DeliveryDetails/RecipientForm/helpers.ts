dc75d18ed37c119634a80a1e0fa1f6a47aa3eb0c
m4tewoosh <mnowak@cleeng.com>
2025-06-04 11:21:32 +0200

    feat/MSSDK-2460: fix showing delivery date and time in correct timezone

--- src/components/DeliveryDetails/RecipientForm/helpers.ts
+++ src/components/DeliveryDetails/RecipientForm/helpers.ts
@@ -1,7 +1,14 @@
-const getDate = (date: number) =>
-  new Date(date * 1000).toISOString().split('T')[0];
-
+const getDate = (date: number): string => {
+  const localDate = new Date(date * 1000);
+  const year = localDate.getFullYear();
+  const month = (localDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = localDate.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
 const getTime = (date: number) =>
-  new Date(date * 1000).toISOString().split('T')[1].substring(0, 5);
+  new Date(date * 1000).toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
 
 export { getDate, getTime };
