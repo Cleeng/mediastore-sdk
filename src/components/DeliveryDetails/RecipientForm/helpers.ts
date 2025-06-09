8ca0945a31962fc12f1dac6b48ea029d607a508b
Mateusz Nowak <68298935+m4tewoosh@users.noreply.github.com>
2025-06-09 16:44:15 +0200

    feat/MSSDK-2460: fix showing delivery date and time in correct timezone (#507)
    * feat/MSSDK-2460: fix showing delivery date and time in correct timezone
    
    * fix/MSSDK-2460: remove returned value
    
    * feat/MSSDK-2460: change getTime function
    
    * feat/MSSDK-2460: mini refactor

--- src/components/DeliveryDetails/RecipientForm/helpers.ts
+++ src/components/DeliveryDetails/RecipientForm/helpers.ts
@@ -1,7 +1,18 @@
-const getDate = (date: number) =>
-  new Date(date * 1000).toISOString().split('T')[0];
+const getDate = (date: number) => {
+  const localDate = new Date(date * 1000);
+  const year = localDate.getFullYear();
+  const month = (localDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = localDate.getDate().toString().padStart(2, '0');
 
-const getTime = (date: number) =>
-  new Date(date * 1000).toISOString().split('T')[1].substring(0, 5);
+  return `${year}-${month}-${day}`;
+};
+
+const getTime = (date: number) => {
+  const localDate = new Date(date * 1000);
+  const hours = localDate.getHours().toString().padStart(2, '0');
+  const minutes = localDate.getMinutes().toString().padStart(2, '0');
+
+  return `${hours}:${minutes}`;
+};
 
 export { getDate, getTime };
